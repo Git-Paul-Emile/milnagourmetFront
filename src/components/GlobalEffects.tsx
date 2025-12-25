aa4608e6d6a47b1bb164de5d4f5be824dfd531b7
PECKOUET-BINOMBO <apeckouet@gmail.com>
2025-12-25 17:32:28 +0000

    ajout du theme nouvel an2

--- src/components/GlobalEffects.tsx
+++ src/components/GlobalEffects.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useTheme } from '@/hooks/useTheme';
 import ChristmasSnow from './ChristmasSnow';
+import NewYearEffects from './NewYearEffects';
 
 export function GlobalEffects() {
   const { theme } = useTheme();
   const isChristmasTheme = theme?.name === 'Noël';
+  const isNewYearTheme = theme?.name === 'Nouvel An';
 
   return (
     <>
       {isChristmasTheme && <ChristmasSnow />}
+      {isNewYearTheme && <NewYearEffects />}
     </>
   );
 }
